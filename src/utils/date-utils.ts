bbbd3dd7ab7ca0397b7010d78255cdebb8fdd250
Julio Canizalez <juliocanizalez@outlook.com>
2025-02-10 10:43:21 -0600

    Add date utils

---
+++ src/utils/date-utils.ts
@@ -0,0 +1,10 @@
+export const formatDate = (dateString: string): string => {
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  });
+
+  return `${formattedDate}`;
+};
