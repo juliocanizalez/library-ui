fd9be32735aab7ad66b04b514dfd13860274477a
Julio Canizalez <juliocanizalez@outlook.com>
2025-02-10 11:55:48 -0600

    Update checkout interface

--- src/interfaces/ICheckout.ts
+++ src/interfaces/ICheckout.ts
@@ -1,9 +1,10 @@
 export interface ICheckout {
-  user: number;
-  user_email: string;
-  book_ids: number[];
-  book_title: string;
-  checkout_date: string;
-  return_date: string;
-  is_returned: boolean;
+  user?: number;
+  user_email?: string;
+  book_ids?: number[];
+  book_title?: string;
+  checkout_date?: string;
+  return_date?: string;
+  is_returned?: boolean;
+  books?: number[];
 }
